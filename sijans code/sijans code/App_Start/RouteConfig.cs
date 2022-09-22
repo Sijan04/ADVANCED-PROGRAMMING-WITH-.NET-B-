af4e5a69cc911d1156383f678a8b031564acff18
sijan <sijansidda4@gmail.com>
2022-09-23 00:59:09 +0600

    CV

---
+++ sijans code/sijans code/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace sijans_code
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "fifa",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "my", action = "Intro", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ sijans code/sijans code/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sijans_code.Global" Language="C#" %>
